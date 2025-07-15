fcfff24ab43973bba6ac9d24892e52a8a60760fb
AHmeDNasR54 <ahmed17nasr54@gmail.com>
2025-07-15 06:05:15 +0300

    Add project files.

---
+++ Password-App/Encriptio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password_App
+{
+    public class EncriptionUtilize
+    {
+        private static readonly string origionalChars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "0123456789" +
+            "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\`~";
+        private static readonly string altChars =
+                "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz" +
+                "0123456789" +
+                "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\`~";
+
+        public static string Encript(string password)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in password)
+            {
+                var charIndex = origionalChars.IndexOf(c);
+                sb.Append(altChars[charIndex]);
+
+
+            }
+            return sb.ToString();
+        }
+        public static string Decript(string password)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in password)
+            {
+                var charIndex = altChars.IndexOf(c);
+                sb.Append(origionalChars[charIndex]);
+
+
+            }
+            return sb.ToString();
+        }
+    }
---
+++ PasswordTrainningApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using PasswordTrainningApp.PasswordManager;
+
+namespace PasswordTrainningApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            App.Run(args);
+          /* string allChars =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ" +
+            "abcdefghijklmnopqrstuvwxyz" +
+            "0123456789" +
+            "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\`~";
+            string alltChars =
+                "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz" +
+                "0123456789" +
+                "!@#$%^&*()-_=+[]{}|;:'\",.<>?/\\`~";
+
+            Console.WriteLine(allChars.Length);
+            Console.WriteLine(alltChars.Length);
+            var missingChars = allChars.Except(alltChars);
+
+            Console.WriteLine("Missing character(s): " + string.Join(", ", missingChars));
+       */
+       }
+    
+    }
+}
